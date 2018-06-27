e0312bf52503ad3a1944a665e5a488dd617a6224
Franklin Marmon <agathezol@stirgegaming.com>
2018-06-27 16:13:02 -0600

    Updated stats reporter icon to a more appropriate look. Fixed ordinal use in stats reporter output.

--- src/main/java/com/stirgegaming/mudmod/core/events/EventsHandler.java
+++ src/main/java/com/stirgegaming/mudmod/core/events/EventsHandler.java
@@ -1,17 +1,15 @@
 package com.stirgegaming.mudmod.core.events;
 
-import com.mojang.realmsclient.gui.ChatFormatting;
 import com.stirgegaming.mudmod.core.stats.MMPlayerStats;
 import com.stirgegaming.mudmod.core.util.StringUtils;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-
-import java.util.Iterator;
 
 public class EventsHandler {
 
@@ -24,8 +22,8 @@ public class EventsHandler {
                 mmps.GetPlayerStats(((EntityPlayerMP)player));
                 for( EntityPlayer e : ((EntityPlayerMP)player).getServerWorld().playerEntities )  {
                     e.sendMessage( new TextComponentString( player.getName()+" "+
-                            ChatFormatting.RED+"died"+ChatFormatting.WHITE+" for the "+
-                            StringUtils.ordinal(mmps.deaths+1)+ " time having "+ChatFormatting.GREEN+"destroyed"+ChatFormatting.WHITE+
+                            TextFormatting.RED+"died"+TextFormatting.WHITE+" for the "+
+                            StringUtils.ordinal(mmps.deaths+1)+ " time having "+TextFormatting.GREEN+"destroyed"+TextFormatting.WHITE+
                             " "+mmps.mobkills+" monsters."));
                 }
                 mmps.WritePlayerStats(((EntityPlayerMP)player));
--- src/main/java/com/stirgegaming/mudmod/items/ItemStatsClipboard.java
+++ src/main/java/com/stirgegaming/mudmod/items/ItemStatsClipboard.java
@@ -1,7 +1,5 @@
 package com.stirgegaming.mudmod.items;
 
-import com.mojang.realmsclient.gui.ChatFormatting;
-import com.stirgegaming.mudmod.MudMod;
 import com.stirgegaming.mudmod.core.stats.MMPlayerStats;
 import com.stirgegaming.mudmod.core.util.StringUtils;
 import net.minecraft.creativetab.CreativeTabs;
@@ -12,6 +10,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
 
 public class ItemStatsClipboard extends Item {
@@ -31,11 +30,11 @@ public class ItemStatsClipboard extends Item {
             mmps.GetPlayerStats((EntityPlayerMP) player);
 
             for (EntityPlayer e : ((EntityPlayerMP) player).getServerWorld().playerEntities) {
-                e.sendMessage(new TextComponentString(player.getName() + " has: "+ChatFormatting.RED + "died" + ChatFormatting.RESET+ " " +
-                        StringUtils.ordinal(mmps.deaths) + " times "));
-                e.sendMessage(new TextComponentString( "     "+ChatFormatting.GREEN + "destroyed" + ChatFormatting.RESET+
+                e.sendMessage(new TextComponentString(player.getName() + " has: "+TextFormatting.RED + "died" + TextFormatting.RESET+ " " +
+                        mmps.deaths + " time"+(mmps.deaths == 1 ? " " : "s ")));
+                e.sendMessage(new TextComponentString( "     "+TextFormatting.GREEN + "destroyed" + TextFormatting.RESET+
                         " " + mmps.mobkills + " monsters"));
-                e.sendMessage(new TextComponentString( "     "+ChatFormatting.YELLOW+ "moved " + ChatFormatting.RESET+
+                e.sendMessage(new TextComponentString( "     "+TextFormatting.YELLOW+ "moved " + TextFormatting.RESET+
                         " " + mmps.steps + " meters"));
             }
 
